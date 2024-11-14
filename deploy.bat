5c87babd7daef0a60e8721b07fc7768ada526e95
amirmahdi264 <amirdashti264@gmail.com>
2024-11-15 00:50:04 +0330

    auto

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,6 @@
-mkdir ../core
-copy * ../core
-
-"C:\Program Files\WinRAR\rar.exe" a ../instagram.rar ../core
-
+set /p USER_INPUT=project: 
+"C:\Program Files\WinRAR\rar.exe" a ../%USER_INPUT%.rar %USER_INPUT%
+cd %USER_INPUT%
 git add .
 git commit -m"auto"
 git push 
