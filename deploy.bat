d4bf77cad59cb33492b1c241f44c72e4063c222b
amirmahdi264 <amirdashti264@gmail.com>
2024-11-15 00:46:55 +0330

    auto

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,8 @@
-"C:\Program Files\WinRAR\rar.exe" a ../instagram.rar *
+mkdir ../core
+copy * ../core
+
+"C:\Program Files\WinRAR\rar.exe" a ../instagram.rar ../core
+
 git add .
 git commit -m"auto"
 git push 
